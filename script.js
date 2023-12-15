92e1ecee83e17b4707265a060cb6cf6c0336dbab
Negatseg <tseganehnegash@gmail.com>
2023-12-15 13:25:38 -0600

    update html and javascript file

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 
-let timer = 60;
+let timer;
+let secondsRemaining = 60;
 let score = 0;
 let currentQuestionIndex = 0;
 
 document.getElementById("start-button").addEventListener("click", () => {
+  document.getElementById("top").style.display="none";
+  document.getElementById("middle").style.display="block";
   startTimer();
+
 });
 
 
@@ -13,130 +17,161 @@ document.getElementById("start-button").addEventListener("click", () => {
 
 
 document.getElementById("quizForm").addEventListener("submit", function (e) {
-    e.preventDefault(); 
-  
+  e.preventDefault();
+
+
+  const selectedAnswer = document.querySelector("input[name='datatype']:checked");
+
+  if (selectedAnswer) {
+    const userAnswer = selectedAnswer.value;
+    const correctAnswer = "alerts";
+
+
+    if (userAnswer === correctAnswer) {
+      alert("Correct!");
+      score++;
+    } else {
+      alert("Incorrect. The correct answer is alerts.");
+      secondsRemaining-=10
+    }
+
+    document.getElementById("quizForm").style.display="none";
     
-    const selectedAnswer = document.querySelector("input[name='datatype']:checked");
-  
-    if (selectedAnswer) {
-      const userAnswer = selectedAnswer.value;
-      const correctAnswer = "alerts"; 
-  
-      
-      if (userAnswer === correctAnswer) {
-        alert("Correct!");
-        score++;
-      } else {
-        alert("Incorrect. The correct answer is alerts.");
-      }
-    } else {
-      alert("Please select an answer.");
-    }
-  });
-
-  document.getElementById("quizForm1").addEventListener("submit", function (e) {
-    e.preventDefault(); 
-  
-  
-    const selectedAnswer = document.querySelector("input[name='enclosed']:checked");
-  
-    if (selectedAnswer) {
-      const userAnswer = selectedAnswer.value;
-      const correctAnswer = "parenthesis"; 
-  
-      
-      if (userAnswer === correctAnswer) {
-        alert("Correct!");
-        score++;
-      } else {
-        alert("Incorrect. The correct answer is parenthesis.");
-      }
-    } else {
-      alert("Please select an answer.");
-    }
-  });
-
-  document.getElementById("quizForm2").addEventListener("submit", function (e) {
-    e.preventDefault(); 
-  
+    document.getElementById("quizForm1").style.display="block";
     
-    const selectedAnswer = document.querySelector("input[name='store']:checked");
-  
-    if (selectedAnswer) {
-      const userAnswer = selectedAnswer.value;
-      const correctAnswer = "All of the above"; 
-  
-      
-      if (userAnswer === correctAnswer) {
-        alert("Correct!");
-        score++;
-      } else {
-        alert("Incorrect. The correct answer is All of the above.");
-      }
-    } else {
-      alert("Please select an answer.");
-    }
-  });
-
-  document.getElementById("quizForm3").addEventListener("submit", function (e) {
-    e.preventDefault();
-  
     
-    const selectedAnswer = document.querySelector("input[name='assigned']:checked");
-  
-    if (selectedAnswer) {
-      const userAnswer = selectedAnswer.value;
-      const correctAnswer = "quotes";
-  
-      
-      if (userAnswer === correctAnswer) {
-        alert("Correct!");
-        score++;
-      } else {
-        alert("Incorrect. The correct answer is quotes");
-      }
-    } else {
-      alert("Please select an answer.");
-    }
-  });
-
-
-  document.getElementById("quizForm4").addEventListener("submit", function (e) {
-    e.preventDefault(); 
-  
-    
-    const selectedAnswer = document.querySelector("input[name='debugging']:checked");
-  
-    if (selectedAnswer) {
-      const userAnswer = selectedAnswer.value;
-      const correctAnswer = "console.log"; 
-  
-      
-      if (userAnswer === correctAnswer) {
-        alert("Correct!");
-        score++;
-      } else {
-        alert("Incorrect. The correct answer is console.log");
-      }
-    } else {
-      alert("Please select an answer.");
-    }
-  });
-
-  let secondsRemaining = 60; 
-
-  function startTimer() {
-    timer = setInterval(function () {
-      secondsRemaining--;
-      const timerElement = document.getElementById("timer");
-      timerElement.textContent = `Time Left: ${secondsRemaining} seconds`;
-  
-      if (secondsRemaining <= 0) {
-        clearInterval(timer);
-        alert("Time's up!");
-        showNextQuestion();
-      }
-    }, 1000);
+  } else {
+    alert("Please select an answer.");
   }
+});
 
- 
+document.getElementById("quizForm1").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+
+  const selectedAnswer = document.querySelector("input[name='enclosed']:checked");
+
+  if (selectedAnswer) {
+    const userAnswer = selectedAnswer.value;
+    const correctAnswer = "parenthesis";
+
+
+    if (userAnswer === correctAnswer) {
+      alert("Correct!");
+      score++;
+    } else {
+      alert("Incorrect. The correct answer is parenthesis.");
+      secondsRemaining-=10
+    }
+
+    document.getElementById("quizForm1").style.display="none";
+    
+    document.getElementById("quizForm2").style.display="block";
+  } else {
+    alert("Please select an answer.");
+  }
+});
+
+document.getElementById("quizForm2").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+
+  const selectedAnswer = document.querySelector("input[name='store']:checked");
+
+  if (selectedAnswer) {
+    const userAnswer = selectedAnswer.value;
+    const correctAnswer = "All of the above";
+
+
+    if (userAnswer === correctAnswer) {
+      alert("Correct!");
+      score++;
+    } else {
+      alert("Incorrect. The correct answer is All of the above.");
+      secondsRemaining-=10
+    }
+    document.getElementById("quizForm2").style.display="none";
+    
+    document.getElementById("quizForm3").style.display="block";
+  } else {
+    alert("Please select an answer.");
+  }
+});
+
+document.getElementById("quizForm3").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+
+  const selectedAnswer = document.querySelector("input[name='assigned']:checked");
+
+  if (selectedAnswer) {
+    const userAnswer = selectedAnswer.value;
+    const correctAnswer = "quotes";
+
+
+    if (userAnswer === correctAnswer) {
+      alert("Correct!");
+      score++;
+    } else {
+      alert("Incorrect. The correct answer is quotes");
+      secondsRemaining-=10
+    }
+
+    document.getElementById("quizForm3").style.display="none";
+    
+    document.getElementById("quizForm4").style.display="block";
+  } else {
+    alert("Please select an answer.");
+  }
+});
+
+
+document.getElementById("quizForm4").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+
+  const selectedAnswer = document.querySelector("input[name='debugging']:checked");
+
+  if (selectedAnswer) {
+    const userAnswer = selectedAnswer.value;
+    const correctAnswer = "console.log";
+
+
+    if (userAnswer === correctAnswer) {
+      alert("Correct!");
+      score++;
+    } else {
+      alert("Incorrect. The correct answer is console.log");
+      secondsRemaining-=10
+    }
+   endQuiz()
+  } else {
+    alert("Please select an answer.");
+  }
+});
+
+
+
+function startTimer() {
+  timer = setInterval(function () {
+    secondsRemaining--;
+    const timerElement = document.getElementById("timer");
+    timerElement.textContent = `Time Left: ${secondsRemaining} seconds`;
+
+    if (secondsRemaining <= 0) {
+      endQuiz();
+      //showNextQuestion();
+    }
+  }, 1000);
+}
+
+
+function endQuiz() {
+  clearInterval(timer);
+  document.getElementById("bottom").style.display="block";
+  document.getElementById("middle").style.display="none";
+  document.getElementById("final-score").textContent=score
+}
+
+
+
